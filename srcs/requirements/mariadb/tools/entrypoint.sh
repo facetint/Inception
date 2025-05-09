9b3659be850319ed4a751703085cb9ed47e1e95c
facetint <fatmanurcetintas7@gmail.com>
2025-05-09 06:59:01 +0300

    feat(mariadb): add automated entrypoint script for initializing and configuring MariaDB

---
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+initialize_database() {
+    rm -rf /var/lib/mysql/*
+    mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+}
+
+start_mariadb() {
+    echo "Starting MariaDB in the background"
+    mariadbd --user=mysql --datadir=/var/lib/mysql --console &
+    sleep 5
+}
+
+generate_sql_config() {
+    echo "⚙️ Generating SQL configuration..."
+    cat << EOF > /tmp/sqlConfig.sql
+    CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+    CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+    GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+
+    CREATE USER IF NOT EXISTS '${DB_ADMIN_USER}'@'%' IDENTIFIED BY '${DB_ADMIN_PASS}';
+    GRANT ALL PRIVILEGES ON *.* TO '${DB_ADMIN_USER}'@'%' WITH GRANT OPTION;
+
+    FLUSH PRIVILEGES;
+EOF
+}
+
+apply_sql_config() {
+    echo "Applying SQL configuration..."
+    mysql < /tmp/sqlConfig.sql
+    echo "SQL configuration applied successfully."
+}
+
+restart_mariadb() {
+    echo "Restarting MariaDB..."
+    pkill -9 "mariadbd"
+    sleep 2
+    exec mariadbd --user=mysql --datadir=/var/lib/mysql --port=3306 --console
+}
+
+main() {
+    echo "Starting MariaDB setup process"
+    initialize_database
+    start_mariadb
+    generate_sql_config
+    apply_sql_config
+    restart_mariadb
+}
+
+main "$@"
